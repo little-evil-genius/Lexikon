61dafa459e0edd5f8a4555735169a2833f4ec649
Lara <75126217+little-evil-genius@users.noreply.github.com>
2023-10-24 11:56:01 +0200

    Create lexicon.lang.php

---
+++ inc/languages/deutsch_du/lexicon.lang.php
@@ -0,0 +1,91 @@
+<?php
+
+$l['lexicon_nav_main'] = "Lexikon";
+$l['lexicon_cat_delet_notice'] = "Sobald du diese Kategorie löscht, löscht du auch alle Einträge innerhalb dieser. Wirklich löschen?";
+
+// HEADERMELDUNGEN
+$l['newentry_lexicon_headerbanner'] = '<div class="red_alert">Es {1} <a href="modcp.php?action=lexicon"><b>{2}</b> {3} {4}</a> fürs Lexikon zum freischalten vor!</div>';
+
+// WEITERLEITUNGSSEITEN
+$l['lexicon_redirect_modcp_delete'] = "Der Eintrag fürs Lexikon wurde erfolgreich abgelehnt und gelöscht! Du wirst nun zurückgeleitet ins Modcp.";
+$l['lexicon_redirect_modcp_accept'] = "Der Eintrag fürs Lexikon wurde erfolgreich angenommen und freigeschaltet! Du wirst nun zurückgeleitet ins Modcp.";
+$l['lexicon_redirect_uninstall'] = "Das boardinterne Lexikon wurde vom Team nicht installiert! Du wirst nun auf die Hauptseite vom Board zurückgeleitet!";
+$l['lexicon_redirect_contents_deaktiv'] = "Das Team hat das Inhaltsverzeichnis deaktiviert! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_add_error_cat'] = "Du hast keine Berechtigung, eine Kategorie dem Lexikon hinzuzufügen! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_add_error_entry'] = "Du hast keine Berechtigung, ein Eintrag dem Lexikon hinzuzufügen! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_add_cat'] = "Die Kategorie wurde erfolgreich dem Lexikon hinzuzufügen! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_add_entry'] = "Der Eintrag wurde erfolgreich dem Lexikon hinzuzufügen! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_edit_error_cat'] = "Du hast keine Berechtigung, eine Kategorie vom Lexikon zu bearbeiten! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_edit_error_entry'] = "Du hast keine Berechtigung, ein Eintrag vom Lexikon zu bearbeiten! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_edit_cat'] = "Die Kategorie wurde erfolgreich bearbeitet! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_edit_entry'] = "Der Eintrag wurde erfolgreich bearbeitet! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_delete_cat'] = "Die Kategorie und alle Einträge innherhalb dieser wurden erfolgreich gelöscht! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_delete_entry'] = "Der Eintrag wurde erfolgreich gelöscht! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+
+// MODCP
+$l['lexicon_modcp_nav'] = "Neue Lexikon Einträge";
+$l['lexicon_modcp'] = "Lexikon Einträge freischalten";
+$l['lexicon_modcp_linktitel'] = "Linktitel:";
+$l['lexicon_modcp_link'] = "Link:";
+$l['lexicon_modcp_sendby'] = "Eingereicht von:";
+$l['lexicon_modcp_entrytext'] = "Der neue Eintrag";
+$l['lexicon_modcp_externallink'] = "Dieser Eintrag ist nur externer Link zur Seite:<br>{1}";
+$l['lexicon_modcp_delete_button'] = "Eintrag ablehnen";
+$l['lexicon_modcp_accept_button'] = "Eintrag annehmen";
+
+// INHALTSVERZEICHNIS
+$l['lexicon_contents'] = "Inhaltsverzeichnis";
+$l['lexicon_contents_desc'] = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ";
+
+// HINZUFÜGEN KATEGORIE
+$l['lexicon_nav_add_category'] = "Kategorie hinzufügen";
+$l['lexicon_nav_edit_category'] = "Kategorie bearbeiten";
+$l['lexicon_add_categoryname_titel'] = "Kategorie-Name";
+$l['lexicon_add_categoryname_desc'] = "Wie lautet der Name der Kategorie?<br>Kategorien werden alphabetisch nach ihren Namen im Menü sortieren oder nach einer manuellen Sortierung, wenn diese aktiviert ist.";
+
+// HINZUFÜGEN & BEARBEITEN EINTRAG
+$l['lexicon_nav_add_entry'] = "Eintrag hinzufügen";
+$l['lexicon_nav_edit_entry'] = "Eintrag bearbeiten";
+$l['lexicon_add_category_titel'] = "Kategorie";
+$l['lexicon_add_category_desc'] = "In welche Kategorie soll dieser Eintrag eingeordnetet werden?";
+$l['lexicon_add_linktitle_titel'] = "Linktitel";
+$l['lexicon_add_linktitle_desc'] = "Wie soll der Linktitel im Menü heißen?<br>
+Dieser Titel ist ähnlich wie ein Schlagwort - kurz und informativ für den ersten Blick.";
+$l['lexicon_add_link_titel'] = "Link";
+$l['lexicon_add_link_desc'] = "Wie soll der Link lauten? lexicon.php?page=linkname<br>
+Bitte auf Umlaute oder Sonderzeichen (_ und - sind gestattet) verzichten. Genau wie Leerzeichen.";
+$l['lexicon_add_externallink_titel'] = "Externer Link";
+$l['lexicon_add_externallink_desc'] = "Soll der Menü-Punkt auf eine externe Seite weiterleiten? Zum Beispiel auf einen Schulrechner?<br> 
+Der Forenlink muss nicht mitangegben werden.";
+$l['lexicon_add_title_titel'] = "Titel";
+$l['lexicon_add_title_desc'] = "Wie lautet der volle Titel des Eintrages?";
+$l['lexicon_add_entrytext'] = "Der Eintrag";
+
+// MAINPAGE
+$l['lexicon_main_desc'] = "Wir wissen, wie überfordernd ein neues Forum und deren Hintergrund sein kann. Damit euch der Einstieg ins Far from Heaven leichter fällt, haben wir ein foreninternes Wiki für euch erstellt.<br>
+<br>
+Solltet ihr immer noch Fragen haben, dann schreibt am besten direkt in den Support und das Team wird euch sicherlich helfen können. Wir werden uns dann auch bemühen, unser Wiki soweit zu ergänzen, damit niemand sich durch den Support klicken muss.
+Sollte euch ein Eintrag oder Ähnliches fehlen, so meldet euch beim Team, damit wir euren Wunsch besprechen und gegebenfalls umsetzen können. Wir freuen uns immer über rege Userbereitschaft und gehen nach dem Prinzip, ein Forum lebt von seinen Usern und deren Ideen. Wir wollen ja eine gemütliche Gemeinschaft erschaffen. ";
+
+// SORTIERUNG
+$l['lexicon_sort_titel'] = "Sortierung";
+$l['lexicon_sort_desc'] = "An welcher Stelle soll der Eintrag/die Kategorie angezeigt werden?";
+
+// UNTEREINTRAG
+$l['lexicon_sub_titel'] = "Untereintrag";
+$l['lexicon_sub_desc'] = "Ist dieser Eintrag einem anderem Eintrag untergordnet?";
+
+// MyALERTS
+$l['lexicon_delete'] = '{1} hat dein Lexikon Eintrag <b>"{3}"</b> abgelehnt!';
+$l['myalerts_setting_lexicon_delete'] = "Benachrichtigung über Ablehnung von eingereichten Lexikoneinträgen?";
+$l['lexicon_accept'] = '{1} hat dein Lexikon Eintrag <b>"{3}"</b> angenommen!';
+$l['myalerts_setting_lexicon_accept'] = "Benachrichtigung über Annahme von eingereichten Lexikoneinträgen?";
+
+// ONLINE LOCATION
+$l['lexicon_online_location_main'] = "Sieht sich das <a href=\"lexicon.php\">Lexikon</a> an.";
+$l['lexicon_online_location_contents'] = "Sieht sich das <a href=\"lexicon.php?action=contents\">Inhaltsverzeichnis vom Lexikon</a> an.";
+$l['lexicon_online_location_add_category'] = "Fügt dem <a href=\"lexicon.php\">Lexikon</a> eine neue Kategorie hinzu.";
+$l['lexicon_online_location_add_entry'] = "Fügt dem <a href=\"lexicon.php\">Lexikon</a> ein neuen Eintrag hinzu.";
+$l['lexicon_online_location_edit_category'] = "Bearbeitet gerade die <a href=\"lexicon.php\">Lexikon</a> Kategorie <b>{1}</b>.";
+$l['lexicon_online_location_edit_entry'] = "Bearbeitet gerade den <a href=\"lexicon.php\">Lexikon</a> Eintrag <b>{1}</b>.";
+$l['lexicon_online_location_page'] = "Sieht sich den <a href=\"lexicon.php?page={1}\">Lexikoneintrag <b>{2}</b></a> an.";
