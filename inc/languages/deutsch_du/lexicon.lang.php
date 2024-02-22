b4ca4da4bf83daca3e85e46935838fa3ae1e8b57
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-02-22 08:49:37 +0100

    Update lexicon.lang.php

--- inc/languages/deutsch_du/lexicon.lang.php
+++ inc/languages/deutsch_du/lexicon.lang.php
@@ -2,7 +2,7 @@
 
 $l['lexicon_nav_main'] = "Lexikon";
 $l['lexicon_cat_delet_notice'] = "Sobald du diese Kategorie löscht, löscht du auch alle Einträge innerhalb dieser. Wirklich löschen?";
-$l['lexicon_entry_delet_notice'] = "Sobald du diesen Eintrag löscht, löscht du auch alle Einträge welchem diesem untergordnet sind. Wirklich löschen?";
+$l['lexicon_entry_delet_notice'] = "Sobald du diesen Eintrag löscht, löscht du auch alle Einträge welche diesem untergordnet sind. Wirklich löschen?";
 $l['lexicon_button_edit'] = "Bearbeiten";
 $l['lexicon_button_delete'] = "Löschen";
 
@@ -10,15 +10,15 @@ $l['lexicon_button_delete'] = "Löschen";
 $l['lexicon_header_banner'] = '<a href="modcp.php?action=lexicon">Es {1} <b>{2}</b> {3} {4} fürs Lexikon zum freischalten vor!</a>';
 
 // WEITERLEITUNGSSEITEN
-$l['lexicon_redirect_modcp_delete'] = "User können keine Einträge hinzufügen, weswegen die Freischaltungsseite im Modcp deaktiviert ist. Du wirst nun zurückgeleitet auf die Hauptseite vom Modcp.";
-$l['lexicon_redirect_modcp_delete'] = "Der Eintrag fürs Lexikon wurde erfolgreich abgelehnt und gelöscht! Du wirst nun zurückgeleitet ins Modcp.";
-$l['lexicon_redirect_modcp_accept'] = "Der Eintrag fürs Lexikon wurde erfolgreich angenommen und freigeschaltet! Du wirst nun zurückgeleitet ins Modcp.";
+$l['lexicon_redirect_modcp_delete'] = "User können keine Einträge hinzufügen, weswegen die Freischaltungsseite im Mod-CP deaktiviert ist. Du wirst nun zurückgeleitet auf die Hauptseite vom Mod-CP.";
+$l['lexicon_redirect_modcp_delete'] = "Der Eintrag fürs Lexikon wurde erfolgreich abgelehnt und gelöscht! Du wirst nun zurückgeleitet ins Mod-CP.";
+$l['lexicon_redirect_modcp_accept'] = "Der Eintrag fürs Lexikon wurde erfolgreich angenommen und freigeschaltet! Du wirst nun zurückgeleitet ins Mod-CP.";
 $l['lexicon_redirect_uninstall'] = "Das boardinterne Lexikon wurde vom Team nicht installiert! Du wirst nun auf die Hauptseite vom Board zurückgeleitet!";
 $l['lexicon_redirect_contents_deaktiv'] = "Das Team hat das Inhaltsverzeichnis deaktiviert! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
 $l['lexicon_redirect_add_error_cat'] = "Du hast keine Berechtigung, eine Kategorie dem Lexikon hinzuzufügen! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
 $l['lexicon_redirect_add_error_entry'] = "Du hast keine Berechtigung, ein Eintrag dem Lexikon hinzuzufügen! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
-$l['lexicon_redirect_add_cat'] = "Die Kategorie wurde erfolgreich dem Lexikon hinzuzufügen! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
-$l['lexicon_redirect_add_entry'] = "Der Eintrag wurde erfolgreich dem Lexikon hinzuzufügen! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_add_cat'] = "Die Kategorie wurde erfolgreich dem Lexikon hinzugefügt! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
+$l['lexicon_redirect_add_entry'] = "Der Eintrag wurde erfolgreich dem Lexikon hinzugefügt! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
 $l['lexicon_redirect_edit_error_cat'] = "Du hast keine Berechtigung, eine Kategorie vom Lexikon zu bearbeiten! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
 $l['lexicon_redirect_edit_error_entry'] = "Du hast keine Berechtigung, ein Eintrag vom Lexikon zu bearbeiten! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
 $l['lexicon_redirect_edit_cat'] = "Die Kategorie wurde erfolgreich bearbeitet! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
@@ -27,17 +27,17 @@ $l['lexicon_redirect_delete_cat'] = "Die Kategorie und alle Einträge innherhalb
 $l['lexicon_redirect_delete_entry'] = "Der Eintrag wurde erfolgreich gelöscht! Du wirst nun auf die Hauptseite vom Lexikon zurückgeleitet!";
 
 // MODCP
-$l['lexicon_modcp_nav'] = "Neue Lexikon Einträge";
-$l['lexicon_modcp'] = "Lexikon Einträge freischalten";
-$l['lexicon_modcp_linktitel'] = "Menuanzeige:";
+$l['lexicon_modcp_nav'] = "Neue Lexikon-Einträge";
+$l['lexicon_modcp'] = "Lexikon-Einträge freischalten";
+$l['lexicon_modcp_linktitel'] = "Menüanzeige:";
 $l['lexicon_modcp_link'] = "Link:";
 $l['lexicon_modcp_sendby'] = "Eingereicht von:";
 $l['lexicon_modcp_entrytext'] = "Der neue Eintrag";
-$l['lexicon_modcp_externallink'] = "Dieser Eintrag ist nur externer Link zur Seite:<br>{1}";
+$l['lexicon_modcp_externallink'] = "Dieser Eintrag ist nur ein externer Link zur Seite:<br>{1}";
 $l['lexicon_modcp_delete_button'] = "Ablehnen";
 $l['lexicon_modcp_accept_button'] = "Annehmen";
 $l['lexicon_modcp_edit_button'] = "Bearbeiten";
-$l['lexicon_modcp_control_none'] = "Momentan gibt es keine Lexikon Einträge zum freischalten.";
+$l['lexicon_modcp_control_none'] = "Momentan gibt es keine Lexikon-Einträge zum freischalten.";
 
 // INHALTSVERZEICHNIS
 $l['lexicon_contents'] = "Inhaltsverzeichnis";
@@ -61,16 +61,21 @@ $l['lexicon_add_link_titel'] = "Link";
 $l['lexicon_add_link_desc'] = "Wie soll der Link lauten? lexicon.php?page=linkname<br>
 Bitte auf Umlaute oder Sonderzeichen (_ und - sind gestattet) verzichten. Genau wie Leerzeichen.";
 $l['lexicon_add_externallink_titel'] = "Externer Link";
-$l['lexicon_add_externallink_desc'] = "Soll der Menü-Punkt auf eine externe Seite weiterleiten? Zum Beispiel auf einen Schulrechner?<br> 
+$l['lexicon_add_externallink_desc'] = "Soll der Menü-Punkt auf eine externe Seite weiterleiten? Zum Beispiel auf einen Schulrechner?<br>
 Der Forenlink muss nicht mitangegben werden.";
 $l['lexicon_add_title_titel'] = "Titel";
-$l['lexicon_add_title_desc'] = "Wie lautet der volle Titel des Eintrages?";
+$l['lexicon_add_title_desc'] = "Wie lautet der volle Titel des Eintrags?";
 $l['lexicon_add_entrytext'] = "Der Eintrag";
 
 // MAINPAGE
 $l['lexicon_main_desc'] = "Herzlich willkommen im foreninternen Lexikon! Hier findest du eine Fülle von Informationen über die Welt, in der unser RPG spielt. Von der Geschichte und Geografie bis hin zu weiteren Hintergrundinformationen - hier ist alles für dich zusammengestellt. Unser Team hat sich viel Mühe gegeben, dieses Lexikon zu erstellen, um dir den Einstieg in unser Forum zu erleichtern.<br>
 <br>
-Falls du Fragen hast oder etwas bestimmtes vermisst, zögere nicht, dich an unser Team zu wenden oder selbst zu erstellen, wenn diese Option freigeschaltet wurde. Wir sind immer bereit, dir weiterzuhelfen und das Lexikon entsprechend zu ergänzen. Denn wir glauben daran, dass ein Forum von der aktiven Beteiligung seiner Mitglieder lebt. Wir freuen uns darauf, mit dir gemeinsam eine lebendige und inspirierende Community aufzubauen. Viel Spaß beim Stöbern und Entdecken!";
+Falls du Fragen hast oder etwas bestimmtes vermisst, zögere nicht, dich an unser Team zu wenden oder selbst zu erstellen, was dir fehlt, wenn diese Option freigeschaltet wurde. Wir sind immer bereit, dir weiterzuhelfen und das Lexikon entsprechend zu ergänzen. Denn wir glauben daran, dass ein Forum von der aktiven Beteiligung seiner Mitglieder lebt. Wir freuen uns darauf, mit dir gemeinsam eine lebendige und inspirierende Community aufzubauen. Viel Spaß beim Stöbern und Entdecken!";
+
+// SUCHE
+$l['lexicon_nav_search'] = "Suchergebnisse für \"{1}\"";
+$l['lexicon_search_input'] = "Suchbegriff eingeben";
+$l['lexicon_search_none'] = "Für diese Suche sind keine Einträge vorhanden.";
 
 // SORTIERUNG
 $l['lexicon_sort_titel'] = "Sortierung";
@@ -81,13 +86,13 @@ $l['lexicon_sub_titel'] = "Untereintrag";
 $l['lexicon_sub_desc'] = "Ist dieser Eintrag einem anderem Eintrag untergordnet?";
 
 // MyALERTS
-$l['lexicon_delete'] = '{1} hat dein Lexikon Eintrag <b>"{3}"</b> abgelehnt!';
+$l['lexicon_delete'] = '{1} hat deinem Lexikon-Eintrag <b>"{3}"</b> abgelehnt!';
 $l['myalerts_setting_lexicon_delete'] = "Benachrichtigung über Ablehnung von eingereichten Lexikoneinträgen?";
-$l['lexicon_accept'] = '{1} hat dein Lexikon Eintrag <b>"{3}"</b> angenommen!';
+$l['lexicon_accept'] = '{1} hat deinen Lexikon-Eintrag <b>"{3}"</b> angenommen!';
 $l['myalerts_setting_lexicon_accept'] = "Benachrichtigung über Annahme von eingereichten Lexikoneinträgen?";
 
 // PN Texte
-$l['lexicon_pm_subject_accept'] = 'Dein Lexikon Eintrag wurde angenommen!';
+$l['lexicon_pm_subject_accept'] = 'Dein Lexikon-Eintrag wurde angenommen!';
 $l['lexicon_pm_message_accept'] = "Hallo {1},<br>vielen Dank für deine Idee für unser Lexikon. Wir freuen uns über deine Kreativität und haben den <a href=\"lexicon.php?page={3}\">Eintrag <b>{2}</b></a> angenommen. Ab sofort wird er im Lexikon angezeigt.<br><br>Alles Liebe";
 $l['lexicon_pm_message_accept_link'] = "Hallo {1},<br>vielen Dank für deine Idee für unser Lexikon. Wir freuen uns über die <a href=\"{3}\">extra verlinkte Seite <b>{2}</b></a> und haben sie angenommen. Ab sofort kann man diese über das Lexikon erreichen.<br><br>Alles Liebe";
 $l['lexicon_pm_subject_delete'] = 'Dein Lexikon Eintrag wurde abgelehnt!';
@@ -99,6 +104,7 @@ $l['lexicon_online_location_main'] = "Sieht sich das <a href=\"lexicon.php\">Lex
 $l['lexicon_online_location_contents'] = "Sieht sich das <a href=\"lexicon.php?action=contents\">Inhaltsverzeichnis vom Lexikon</a> an.";
 $l['lexicon_online_location_add_category'] = "Fügt dem <a href=\"lexicon.php\">Lexikon</a> eine neue Kategorie hinzu.";
 $l['lexicon_online_location_add_entry'] = "Fügt dem <a href=\"lexicon.php\">Lexikon</a> ein neuen Eintrag hinzu.";
-$l['lexicon_online_location_edit_category'] = "Bearbeitet gerade die <a href=\"lexicon.php\">Lexikon</a> Kategorie <b>{1}</b>.";
-$l['lexicon_online_location_edit_entry'] = "Bearbeitet gerade den <a href=\"lexicon.php\">Lexikon</a> Eintrag <b>{1}</b>.";
+$l['lexicon_online_location_edit_category'] = "Bearbeitet gerade die <a href=\"lexicon.php\">Lexikonkategprie</a> <b>{1}</b>.";
+$l['lexicon_online_location_edit_entry'] = "Bearbeitet gerade den <a href=\"lexicon.php\">Lexikoneintrag</a> <b>{1}</b>.";
 $l['lexicon_online_location_page'] = "Sieht sich den <a href=\"lexicon.php?page={1}\">Lexikoneintrag <b>{2}</b></a> an.";
+$l['lexicon_online_location_search'] = "Durchsucht das Lexikon nach dem Begriff <a href=\"lexicon.php?search=results&keyword={1}\">{1}</a>.";
